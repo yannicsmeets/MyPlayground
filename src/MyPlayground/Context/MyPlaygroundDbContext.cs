4e24e07d1db74bb6c43481bb17dffdd910c0eae9
yannicsmeets <yannic.smeets@me.com>
2015-09-20 16:08:57 +0200

    Renamed table names for identity stuff

--- src/MyPlayground/Context/MyPlaygroundDbContext.cs
+++ src/MyPlayground/Context/MyPlaygroundDbContext.cs
@@ -4,7 +4,7 @@ using MyPlayground.Models;
 
 namespace MyPlayground.Context
 {
-  public class MyPlaygroundDbContext : IdentityDbContext<ApplicationUser>
+  public class MyPlaygroundDbContext : IdentityDbContext<User>
   {
     protected override void OnModelCreating(ModelBuilder builder)
     {
@@ -12,6 +12,24 @@ namespace MyPlayground.Context
       // Customize the ASP.NET Identity model and override the defaults if needed.
       // For example, you can rename the ASP.NET Identity table names and more.
       // Add your customizations after calling base.OnModelCreating(builder);
+
+      builder.Entity<User>()
+        .ToTable("User");
+
+      builder.Entity<Role>()
+        .ToTable("Role");
+
+      builder.Entity<IdentityUserRole<string>>()
+        .ToTable("UserRole");
+
+      builder.Entity<IdentityUserClaim<string>>()
+        .ToTable("UserClaim");
+
+      builder.Entity<IdentityRoleClaim<string>>()
+        .ToTable("RoleClaim");
+
+      builder.Entity<IdentityUserLogin<string>>()
+        .ToTable("UserLogin");
     }
   }
 }
