0c832c217069abb308afe2fd1b7242222198ffea
yannicsmeets <yannic.smeets@me.com>
2015-09-27 20:28:39 +0200

    Removed unused code in HomeController

--- src/MyPlayground/Controllers/HomeController.cs
+++ src/MyPlayground/Controllers/HomeController.cs
@@ -1,35 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNet.Authorization;
 using Microsoft.AspNet.Mvc;
 
 namespace MyPlayground.Controllers
 {
-    public class HomeController : Controller
+  public class HomeController : Controller
+  {
+    public IActionResult Index()
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
-        public IActionResult Error()
-        {
-            return View("~/Views/Shared/Error.cshtml");
-        }
+      return View();
     }
+
+    public IActionResult Error()
+    {
+      return View("~/Views/Shared/Error.cshtml");
+    }
+  }
 }
